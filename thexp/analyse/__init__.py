c3714fb04c7088aff510775f94eefa94af04eaba
sailist <sailist@outlook.com>
2021-03-10 14:54:40 +0800

    del analyse

--- thexp/analyse/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-
-"""
-
-from .charts import Curve, Parallel
-from .querys import Query, BoardQuery, TestsQuery, ExpsQuery, ReposQuery, Q
-from .viewer import TestViewer, ProjViewer, ExpViewer
-
-from .constrain import C
